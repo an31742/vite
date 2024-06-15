0aefc9b07e3a4261b61efeb54b445cf0aa4abaa3
an31742 <2234170284@qq.com>
2024-06-15 17:22:09 +0800

    完善个人信息，待开发面包屑导航

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <div class="project-name">
+          <h4>an的个人小天地</h4>
+        </div>
+        <div>
+          <el-dropdown @command="handleCommand">
+            <el-image :src="obj.url" :fit="fit" />
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item command="user">个人中心</el-dropdown-item>
+                <el-dropdown-item command="loginOut">退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </div>
+      </el-header>
       <el-container>
         <el-aside width="200px">
           <Mymue></Mymue>
@@ -14,24 +29,25 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import Home from "../home.vue";
 import Mymue from "./myMue.vue";
-export default {
-  name: "",
-  components: {
-    Home,
-    Mymue,
-  },
-  data() {
-    return {};
-  },
+import { ElMessage } from "element-plus";
+import { reactive } from "vue";
+const obj = reactive({
+  url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+});
+const handleCommand = (command: string | number | object) => {
+  ElMessage(`click on item ${command}`);
 };
 </script>
 
 <style scoped>
 .el-header {
+  display: flex;
+  justify-content: space-between;
   background-color: #333;
+  align-items: center;
 }
 .el-aside {
   background-color: #545c64;
@@ -40,5 +56,19 @@ export default {
 .el-main {
   padding: 10px;
   height: calc(100vh - 60px);
+}
+.project-name {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  color: #fff;
+  font-family: "Courier New", Courier, monospace;
+}
+
+.el-image {
+  width: 50px;
+  height: 50px;
+  border-radius: 80px;
+  margin-right: 20px;
 }
 </style>
