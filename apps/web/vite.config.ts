e0232f51ede7ca2fa872f9720092d8a68951cfb1
an31742 <2234170284@qq.com>
2026-03-21 15:20:25 +0800

    优化

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,14 +6,22 @@ import Components from "unplugin-vue-components/vite"
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 import vueJsx from "@vitejs/plugin-vue-jsx"
 export default defineConfig({
+  base: "/",
   plugins: [
     vue(),
     vueJsx(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
+      imports: ["vue", "vue-router", "pinia"],
+      dts: "auto-imports.d.ts",
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: "css",
+        }),
+      ],
+      dts: "components.d.ts",
     }),
   ],
   //配置别名
